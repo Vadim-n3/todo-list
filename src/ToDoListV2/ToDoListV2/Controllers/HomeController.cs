426d059d03bfdec204f6aebe713ba869be924248
Vadim-n3 <ironman50@yandex.ru>
2018-08-19 00:34:11 +0300

    Добавил инициализацию БД и функцию добавления записи

---
+++ src/ToDoListV2/ToDoListV2/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ToDoListV2.Models;
+
+namespace ToDoListV2.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        DoingContext db = new DoingContext();
+
+
+        public ActionResult Index()
+        {
+            IEnumerable<Doing> doings = db.Doings;
+            ViewBag.Doings = doings;
+            return View();
+        }
+
+
+        [HttpGet]
+        public ActionResult AddDoing()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public string AddDoing(Doing doing)
+        {
+            db.Doings.Add(doing);
+            // сохраняем в бд все изменения
+            db.SaveChanges();
+            return "Новая запись в планировщик успешно добавлена";
+        }
+        /*public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }*/
+    }
+}
