a9abcb38cee787f473418538444aa1e00afe4230
Vadim-n3 <ironman50@yandex.ru>
2018-09-02 21:57:50 +0300

    Добавил подтверждение удаления задач + некоторые исправления

---
+++ src/TaskList/TaslList/MyDB/TaskDbInitializer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using TaslList.Models;
+
+ 
+namespace TaslList.MyDB
+{
+    public class TaskDbInitializer : DropCreateDatabaseAlways<TaskContext>
+    {
+        protected override void Seed(TaskContext db)
+        {
+            db.Tasks.Add(new Task { TaskDate = new DateTime(2018, 8, 18), TaskString = "Заплатить за интернет", IsTaskDone = true });
+            db.Tasks.Add(new Task { TaskDate = new DateTime(2018, 8, 19), TaskString = "Оплатить штраф в ГАИ", IsTaskDone = false });
+            db.Tasks.Add(new Task { TaskDate = new DateTime(2018, 8, 20), TaskString = "Скосить траву", IsTaskDone = false });
+
+            base.Seed(db);
+        }
+
+    }
+}
